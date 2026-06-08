dc4cc2469d951f0b6dbe95e8982bac45e2c95c45
Mark Safari <markshelby066@gmail.com>
2026-06-08 15:15:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const display = document.querySelector('.display');
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+//keyboard support
+document.addEventListener('keydown', function(e) {
+    const key = e.key;
+
+   if (!isNaN(key) || ['+', '-', '÷', '×'].includes(key)) {
+    appendValue(key);
+} else if (key === 'Enter' || key === '=') {
+    calculateResult();
+} else if (key === 'Backspace') {
+    display.value = display.value.slice(0, -1);
+
+}else if (key === 'Escape') {
+    clearDisplay();
+}
+});
+
+//light and dark mode toggle
+function toggleTheme() {
+    document.body.classList.toggle('dark');
+
+}
+
+//History feature setting
+const historyList = document.getElementById('historyList');
+
+function calculateResult() { 
+    if(!display.value) return;
+    try {
+        //TRANSLATION LAYER
+        //this take the display value and replaces '÷" with '/' and '×' with '*' 
+        let expression = display.value.replace(/÷/g, '').replace(/×/g, '*');  
+        
+        //Now js understands the expression and can evaluate it
+        const result = Function(`'use strict'; return (${expression})`)();
+
+        historyList.innerHTML += `<li>${display.value} = ${result}</li>`;
+        display.value = result;
+}catch {
+        display.value = 'Error';  
+}
+}
