3c99f97d01ecc9f06f1d0f08156acb2b3102eee6
Mark Safari <markshelby066@gmail.com>
2026-06-09 06:23:22 +0300

    changes

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ function calculateResult() {
 }catch {
         display.value = 'Error';  
 }
+}
 function deleteLast() {
     display.value = display.value.slice(0, -1);
 }
-
-}
+lay.value.slice(0, -1);
